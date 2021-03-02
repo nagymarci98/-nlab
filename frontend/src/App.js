a5be2e608764668e25826924c9c2f73bbc1aaee7
nagymarci98 <55578294+nagymarci98@users.noreply.github.com>
2021-03-02 13:10:42 +0100

    Frontend start: Homepage, routing, product details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <>
-      <h1>Welcome to proshop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
